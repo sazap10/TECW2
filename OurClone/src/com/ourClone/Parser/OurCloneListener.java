6d1a1135f7c2734c89a32d825c8ee950fad40cf6
Sachin Pande <sazap10@gmail.com>
2014-03-20 21:36:18 +0000

    more tokens added

--- OurClone/src/com/ourClone/Parser/OurCloneListener.java
+++ OurClone/src/com/ourClone/Parser/OurCloneListener.java
@@ -26,13 +26,18 @@ public class OurCloneListener extends JavaBaseListener {
 	}
 
 	@Override
-	public void enterSwitchLabel(@NotNull JavaParser.SwitchLabelContext ctx) {
+	public void enterSwitch_block(@NotNull JavaParser.Switch_blockContext ctx) {
 		addTokenToString("BEGINSWITCH");
 	}
 
 	@Override
-	public void exitSwitchLabel(@NotNull JavaParser.SwitchLabelContext ctx) {
+	public void exitSwitch_block(@NotNull JavaParser.Switch_blockContext ctx) {
 		addTokenToString("ENDSWITCH");
+	}
+
+	@Override
+	public void enterSwitchLabel(@NotNull JavaParser.SwitchLabelContext ctx) {
+		addTokenToString("CASE");
 	}
 
 	@Override
@@ -69,5 +74,94 @@ public class OurCloneListener extends JavaBaseListener {
 	public void exitMethodDeclaration(
 			@NotNull JavaParser.MethodDeclarationContext ctx) {
 		addTokenToString("ENDMETHOD");
+	}
+
+	@Override
+	public void enterFor_loop(@NotNull JavaParser.For_loopContext ctx) {
+		addTokenToString("BEGINFOR");
+	}
+
+	@Override
+	public void exitFor_loop(@NotNull JavaParser.For_loopContext ctx) {
+		addTokenToString("ENDFOR");
+	}
+
+	@Override
+	public void enterDo_while_loop(@NotNull JavaParser.Do_while_loopContext ctx) {
+		addTokenToString("BEGINDOWHILE");
+	}
+
+	@Override
+	public void exitDo_while_loop(@NotNull JavaParser.Do_while_loopContext ctx) {
+		addTokenToString("ENDDOWHILE");
+	}
+
+	@Override
+	public void enterWhile_loop(@NotNull JavaParser.While_loopContext ctx) {
+		addTokenToString("BEGINWHILE");
+	}
+
+	@Override
+	public void exitWhile_loop(@NotNull JavaParser.While_loopContext ctx) {
+		addTokenToString("ENDWHILE");
+	}
+
+	@Override
+	public void enterTry_statement(@NotNull JavaParser.Try_statementContext ctx) {
+		addTokenToString("BEGINTRY");
+	}
+
+	@Override
+	public void exitTry_statement(@NotNull JavaParser.Try_statementContext ctx) {
+		addTokenToString("ENDTRY");
+	}
+
+	public void enterIf_else_statement(
+			@NotNull JavaParser.If_else_statementContext ctx) {
+		addTokenToString("BEGINIF");
+	}
+
+	@Override
+	public void exitIf_else_statement(
+			@NotNull JavaParser.If_else_statementContext ctx) {
+		addTokenToString("ENDIF");
+	}
+
+	@Override
+	public void enterSynchronized_block(
+			@NotNull JavaParser.Synchronized_blockContext ctx) {
+		addTokenToString("BEGINSYNC");
+	}
+
+	@Override
+	public void exitSynchronized_block(
+			@NotNull JavaParser.Synchronized_blockContext ctx) {
+		addTokenToString("ENDSYNC");
+	}
+
+	@Override
+	public void exitReturn_statement(
+			@NotNull JavaParser.Return_statementContext ctx) {
+		addTokenToString("RETURN");
+	}
+
+	@Override
+	public void enterCatchClause(@NotNull JavaParser.CatchClauseContext ctx) {
+		addTokenToString("BEGINCATCH");
+	}
+
+	@Override
+	public void exitCatchClause(@NotNull JavaParser.CatchClauseContext ctx) {
+		addTokenToString("ENDCATCH");
+	}
+
+	@Override
+	public void enterFinallyBlock(@NotNull JavaParser.FinallyBlockContext ctx) {
+		addTokenToString("BEGINFINALLY");
+	}
+
+	@Override
+	public void exitFinallyBlock(@NotNull JavaParser.FinallyBlockContext ctx) {
+		addTokenToString("ENDFINALLY");
 	}
 }
