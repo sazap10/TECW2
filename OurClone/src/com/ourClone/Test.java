47ec5b41972d815f56e1d19d5d8be3b828d2cae0
Luke Richardson <l.richardson@cs.ucl.ac.uk>
2014-03-26 09:05:27 +0000

    Updated test code. Now runs all 17 test inputs.

--- OurClone/src/com/ourClone/Test.java
+++ OurClone/src/com/ourClone/Test.java
@@ -4,14 +4,14 @@ import java.io.File;
 
 public class Test {
 	public static String TEST_DIR = "tests";
-	public static final int NUM_TESTS = 3;
+	public static final int NUM_TESTS = 17;
 
 	public static void main(String[] args) {
 		String root = System.getProperty("user.dir");
 		String path = root + File.separator + TEST_DIR + File.separator;
 
-		for (int i = 1; i <= NUM_TESTS; i++) {
-			for (int j = i; j <= NUM_TESTS; j++) {
+		for (int i = 0; i <= NUM_TESTS; i++) {
+			for (int j = 0; j <= NUM_TESTS; j++) {
 				String[] files = { path + "test" + i, path + "test" + j };
 				System.out
 						.printf("Test files: %s, %s ", "test" + i, "test" + j);
